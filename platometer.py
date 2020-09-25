8ebb4df952751dc30019e7ffa8abc7f72edda163
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-09-24 22:08:03 -0700

    Additional updates to improve handling output paths

--- platometer.py
+++ platometer.py
@@ -18,6 +18,7 @@ import numpy as np
 import pandas as pd
 import multiprocessing as mp
 
+from os.path import expanduser
 from scipy.signal import medfilt2d
 from scipy import ndimage as ndi
 from sklearn.mixture import GaussianMixture
@@ -52,25 +53,26 @@ class Platometer:
         # DataFrame with final output
         self.colony_data = None
 
-    def get_path_to_output_file(self, extension=None):
+    def get_path_to_output_file(self, path='', extension=''):
 
-        file_name = os.path.basename(self.path_to_image_file)
-        parent_folder = os.path.dirname(os.path.dirname(self.path_to_image_file))
-        data_folder = os.path.join(parent_folder, 'platometer_output')
-
-        if not os.path.exists(data_folder):
-            os.makedirs(data_folder)
-
-        if extension:
-            path_to_output_file = os.path.join(data_folder, file_name + extension)
+        if path:
+            path_to_output_file = path.replace('~', expanduser('~'))
         else:
-            path_to_output_file = data_folder
+            path_to_output_file = self.path_to_image_file + extension
+
+        already_exists = os.path.isfile(path_to_output_file)
+        if already_exists:
+            raise FileExistsError(('File %s already exists. '
+                                   'Please specify a different path or a different extension.' % path_to_output_file))
 
         return path_to_output_file
 
-    def save(self):
+    def save(self, path='', verbose=False):
 
-        path_to_p_file = self.get_path_to_output_file('.p')
+        path_to_p_file = self.get_path_to_output_file(path=path, extension='.p')
+
+        if verbose:
+            print('Saving to %s.' % path_to_p_file)
 
         # (Hacky) Remove the fit_sin lambda to allow pickle to be used
         del self.fit_row['fitfunc']
@@ -79,9 +81,12 @@ class Platometer:
         with open(path_to_p_file, 'wb') as file_handle:
             pickle.dump(self, file_handle)
 
-    def print(self):
+    def print(self, path='', verbose=False):
 
-        path_to_dat_file = self.get_path_to_output_file('.dat')
+        path_to_dat_file = self.get_path_to_output_file(path=path, extension='.dat')
+
+        if verbose:
+            print('Saving to %s.' % path_to_dat_file)
 
         self.colony_data['circularity'] = np.nan
         self.colony_data['flags'] = np.nan
@@ -436,7 +441,9 @@ def estimate_foreground_by_adaptive_thresholding(im_gray_trimmed, block_size=45)
 
 
 def run_platometer(image, save_to_file=False, verbose=True):
-    p = Platometer(image['path'], plate_format=image['plate_format'], verbose=verbose)
+
+    image_path = image['path'].replace('~', expanduser('~'))
+    p = Platometer(image_path, plate_format=image['plate_format'], verbose=verbose)
 
     try:
         p.gray_and_trim()
@@ -499,18 +506,17 @@ if __name__ == '__main__':
 
     for folder in folders[0]:
 
+        folder = folder.replace('~', expanduser('~'))
         print('Processing %s' % folder)
 
-        # Get path to the processed data folder
-        # parent_folder = os.path.dirname(args.path_to_image_folder_list)
-        # sub_folder = '/'.join(folder.split('/')[4:])
-        # data_folder = os.path.join(parent_folder, sub_folder)
+        # Get path to the folder to contain processed data
+        current_date = datetime.datetime.today().strftime('%Y%m%d')
+        sub_folder = 'platometer_' + current_date
+        data_folder = os.path.join(folder, sub_folder)
 
-        data_folder = folder
-
-        # if not os.path.exists(data_folder):
-        #     os.makedirs(data_folder)
-        #     print('Created %s' % data_folder)
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
+            print('Created %s' % data_folder)
 
         # Get list of JPG files in the folder
         jpg_files = [os.path.join(folder, f) for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f.lower().endswith('jpg') and os.path.getsize(os.path.join(folder,f)) > 0]
@@ -558,6 +564,8 @@ if __name__ == '__main__':
 
             # Remove the temp "chunk" file
             os.remove(path_to_this_chunk_file)
+
+        print('Printing data to %s' % data_folder)
 
         # Print all data
         path_to_all_data_file = os.path.join(data_folder, 'all_data.txt')
