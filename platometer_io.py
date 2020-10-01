a5570cd0e20ab74291ce71fe022cc12737f919c7
Gina Turco <gina@calicolabs.com>
2020-10-01 16:33:08 -0700

    pylint code clean up of platometer_io.py mostly added dict.get()

--- platometer_io.py
+++ platometer_io.py
@@ -1,3 +1,4 @@
+""" code to open, save and plot data from platometer.py"""
 import os
 import pickle
 import pandas as pd
@@ -7,9 +8,8 @@ import matplotlib.colors as colors
 
 from matplotlib.colors import LinearSegmentedColormap
 
-
 def save_to_p(data, output_file=None):
-
+    """save data as pickle file"""
     if not output_file:
         output_file = os.path.join(os.getcwd(), 'output.p')
 
@@ -17,7 +17,7 @@ def save_to_p(data, output_file=None):
         pickle.dump(data, handle)
 
 
-def load(file_path, v=3, verbose=True):
+def load(file_path, version=3, verbose=True):
 
     """
     # Loads all objects from a pickle or a HDF5 file
@@ -37,9 +37,9 @@ def load(file_path, v=3, verbose=True):
         output = {}
 
         with pd.HDFStore(file_path) as store:
-            ks = store.keys()
+            fkeys = store.keys()
 
-        for k in ks:
+        for k in fkeys:
             k = k.lstrip('\/')
             if verbose:
                 print(k)
@@ -49,14 +49,14 @@ def load(file_path, v=3, verbose=True):
 
     elif file_extension == '.p':
 
-        if v == 3:
+        if version == 3:
             output = pd.read_pickle(file_path)
         else:
-            f = open(file_path, 'r')
-            output = pickle.load(f)
+            pickle_file = open(file_path, 'r')
+            output = pickle.load(pickle_file)
 
         if verbose:
-            print(', '.join([k for k in output.keys()]))
+            print(', '.join(output.keys()))
         return output
 
     else:
@@ -64,6 +64,7 @@ def load(file_path, v=3, verbose=True):
 
 
 def plot_plate(data, colorbar=False, **kwargs):
+    """plot colony quantification of plates with colonies color coded by size"""
     plate = np.zeros((32, 48)) + np.nan
 
     rows = data['row'].values.astype(int)
@@ -77,20 +78,9 @@ def plot_plate(data, colorbar=False, **kwargs):
     else:
         fig, ax = plt.subplots(1, 1, figsize=(20, 10))
 
-    if 'vmin' in kwargs:
-        vmin = kwargs['vmin']
-    else:
-        vmin = np.nanpercentile(vals, 5)
-
-    if 'vmax' in kwargs:
-        vmax = kwargs['vmax']
-    else:
-        vmax = np.nanpercentile(vals, 95)
-
-    if 'midrange' in kwargs:
-        midrange = kwargs['midrange']
-    else:
-        midrange = np.percentile(vals[(vals >= vmin) & (vals <= vmax)], [40, 60])
+    vmin = kwargs.get('vmin',np.nanpercentile(vals, 5))
+    vmax = kwargs.get('vmax',np.nanpercentile(vals, 95))
+    midrange = kwargs.get('midrange',np.percentile(vals[(vals >= vmin) & (vals <= vmax)], [40, 60]))
 
     if 'ticklabels' in kwargs:
         xticklabels = kwargs['ticklabels']
@@ -99,7 +89,7 @@ def plot_plate(data, colorbar=False, **kwargs):
         xticklabels = False
         yticklabels = False
 
-    im = ax.imshow(plate, cmap=red_green(),
+    img = ax.imshow(plate, cmap=red_green(),
                    norm=MidpointRangeNormalize(midrange=midrange),
                    interpolation='nearest',
                    vmin=vmin, vmax=vmax)
@@ -117,15 +107,15 @@ def plot_plate(data, colorbar=False, **kwargs):
         ax.set_yticks([])
 
     if colorbar:
-        plt.colorbar(im, ax=ax)
+        plt.colorbar(img, ax=ax)
 
     plt.tight_layout()
 
-    return im, ax
+    return img, ax
 
 
 def red_green():
-
+    """create colormap with range red (low) to green (high)"""
     color_dict = {'red': ((0.0, 0.0, 1.0), (0.5, 0.0, 0.0), (1.0, 0.0, 0.0)),
                   'green': ((0.0, 0.0, 0.0), (0.5, 0.0, 0.0), (1.0, 1.0, 1.0)),
                   'blue': ((0.0, 0.0, 0.0), (1.0, 0.0, 0.0))
@@ -138,13 +128,12 @@ def red_green():
 
 
 class MidpointRangeNormalize(colors.Normalize):
-
+    """normalize colors by data midpoints"""
     def __init__(self, vmin=None, vmax=None, midrange=None, clip=False):
         self.midrange = midrange
         colors.Normalize.__init__(self, vmin, vmax, clip)
 
     def __call__(self, value, clip=None):
-        x = [self.vmin, self.midrange[0], self.midrange[1], self.vmax]
-        y = [0, 0.5, 0.5, 1]
-        return np.ma.masked_array(np.interp(value, x, y))
-
+        x_values = [self.vmin, self.midrange[0], self.midrange[1], self.vmax]
+        y_values = [0, 0.5, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x_values, y_values))
